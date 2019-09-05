46008e507a5e0847d7deecb44d2b4427adcbc7ed
lch8798 <lch8798@gmail.com>
2019-09-05 22:51:21 +0900

    add. init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # catchmind-server
 catchmind-server
+
+귀여운 라즈베리파이 성능 테스트
---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+const server = http.createServer();
+const io = require('socket.io')(server);
+
+let paint = [];
+
+io.on('connection', (socket) => {
+
+	// 방문자 랜더링
+	socket.on('drawInit', () => {
+		socket.emit('drawInit', paint);
+	});
+
+	// 실시간 랜더링
+	socket.on('draw', (data) => {
+		paint.push(data);
+		io.emit('draw', data);
+	});
+
+	// 캔버스 초기화
+	socket.on('paintInit', (data) => { 
+		paint = []; 
+		io.emit('paintInit', paint);
+	});
+});
+
+const port = 3000;
+server.listen(port, () => {
+	    console.log('server listening on port ' + port);
+});
